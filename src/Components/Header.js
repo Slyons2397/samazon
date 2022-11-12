16262b37cc83c534e624c76900d86278c4815b7e
SamLyons <SamLyons@Samuels-MacBook-Air.local>
2022-11-12 15:01:49 -0500

    created project

---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Col, Row } from 'reactstrap';
+
+const Header = () => {
+
+};
+
+export default Header;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,9 @@
+import Header from "../Components/Header";
+const HomePage = () => {
+    return(
+        <div>
+            <Header></Header>
+        </div>
+    )
+}
+export default HomePage;
